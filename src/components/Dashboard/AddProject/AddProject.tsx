2a377709567e58ea9399816c0018012ba0fe1ac7
Mikhail <mmmffjs7438g@gmail.com>
2020-04-02 21:35:40 +0300

    02.04.2020

--- src/components/Dashboard/AddProject/AddProject.tsx
+++ src/components/Dashboard/AddProject/AddProject.tsx
@@ -136,7 +136,8 @@ export const AddProject = () => {
 		Array<Employee>
 	>([initEmployee]);
 	const [employee, setEmployee] = useState<Employee>(initEmployee);
-	
+	const [eventAddGroup, doEventAddGroup] = useState<boolean>(false);
+	const [groupAdded, setGroupAdded] = useState<boolean>(false);
 	const [eventAddPeople, doEventAddPeople] = useState<boolean>(false);
 	const [employeeAdded, setEmployeeAdded] = useState<boolean>(false);
 	const [groupName, changeGroupName] = useState<string>('');
@@ -392,7 +393,78 @@ export const AddProject = () => {
 						Add people
 					</Button>
 				</div>
-				----{eventAddGroup && !groupAdded && }
+				{eventAddGroup && !groupAdded && (
+					<div className={classes.addGroup}>
+						<TextField
+							id="outlined-basic"
+							label="Group name"
+							variant="outlined"
+							value={groupName}
+							onChange={e => changeGroupName(e.target.value)}
+						/>
+						<Autocomplete
+							className={classes.addGroup_Autocomplete}
+							multiple
+							id="tags-standard"
+							options={usersList}
+							getOptionLabel={(option): string =>
+								`${option.Attributes[2].Value} (${option.Attributes[3].Value})`
+							}
+							defaultValue={[]}
+							renderInput={params => (
+								<TextField
+									{...params}
+									variant="outlined"
+									label="Select people"
+									placeholder="Next"
+								/>
+							)}
+							/* onInputChange={(event: object, value: string, reason: string) => {debugger}}
+							 */ onChange={(event: object, value, reason: string) => {
+								const listEmployee = value.map(el => ({
+									sub: el.Attributes[0].Value,
+									name: el.Attributes[2].Value,
+									email: el.Attributes[3].Value,
+								}));
+
+								changeListSelectedEmployee(listEmployee);
+							}}
+						/>
+						<Autocomplete
+							/* 						  className={classes.addGroup_Autocomplete}*/
+							id="tags-standard"
+							options={listPermissions}
+							getOptionLabel={(option): any => option}
+							renderInput={params => (
+								<TextField {...params} variant="outlined" label="Select permission" />
+							)}
+							/* onInputChange={(event: object, value: string, reason: string) => {debugger}}
+							 */
+
+							onChange={(event: object, value, reason: string) =>
+								changeSelectedPermission(value)
+							}
+						/>
+						<Button
+							variant="contained"
+							color="primary"
+							disableElevation
+							onClick={() => {
+								const group: GroupEmployee = {
+									groupName: groupName,
+									listEmployee: listSelectedEmployee,
+									permissions: selectedPermission,
+								};
+								changeListGroup([...listGroup, group]);
+								setGroupAdded(true);
+								doEventAddGroup(false);
+								doEventAddPeople(false);
+							}}
+						>
+							ADD
+						</Button>
+					</div>
+				)}
 				{groupAdded && <span>Group successful added!</span>}
 				{eventAddPeople && !employeeAdded && (
 					<div className={classes.addPeople}>
